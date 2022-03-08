899e21fb3e820c829fb16c8aae8cffd034f21033
Romualdo VIllalobos <romualdorojo97@gmail.com>
2022-03-08 06:34:50 -0500

    Add files via upload

---
+++ OrbitaPhysics.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitaPhysics : MonoBehaviour
+{
+    [SerializeField] private float mass = 1;
+    [SerializeField] private Vector3 velocity;
+    private Vector3 acceleration;
+
+    [Header("Other object")]
+    [SerializeField] private Transform target;
+    [SerializeField] private float targetMass = 1;
+
+    void Update()
+    {
+        acceleration = Vector3.zero;
+
+        // Calculate the gravitational attraction force
+        Vector3 difference = target.position - transform.position;
+        float distance = difference.magnitude;
+        float attractionForceScalar = (mass * targetMass) / distance * distance;
+        Vector3 force = attractionForceScalar * difference.normalized;
+        ApplyForce(force);
+
+        Move();
+    }
+
+    private void Move(){
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+        //// Debug my vectors
+        //transform.position.Draw(Color.red);
+        //acceleration.Draw(position, Color.green);
+        //velocity.Draw(position, Color.blue);
+    }
+
+    private void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+}
