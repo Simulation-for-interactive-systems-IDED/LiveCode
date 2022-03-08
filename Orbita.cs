2827c349dd8580bae368e382101167d7f6a51f14
Romualdo VIllalobos <romualdorojo97@gmail.com>
2022-03-08 06:23:21 -0500

    Add files via upload

---
+++ Orbita.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbita : MonoBehaviour
+{
+    [SerializeField] private float accelerationMagnitude;
+    [SerializeField] private Vector3 velocity;
+    [SerializeField] private Transform target;
+    [SerializeField, Range(0f, 100f)] private float speedLimit;
+
+    void FixedUpdate()
+    {
+        MoveAtTarget();
+    }
+
+    private void MoveAtTarget(){
+        Vector3 acceleration = (target.position - transform.position).normalized * accelerationMagnitude;
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+        
+        
+        // if(velocity.magnitude > speedLimit)
+        // {
+        //     velocity = velocity.normalized * speedLimit;
+        // }
+    }
+}
---
+++ PhysicMoverFluid.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyMath;
+
+public class PhysicMoverFluid : MonoBehaviour
+{
+    private Vector3 acceleration;
+    private Vector3 velocity;
+
+    private float gravity = -9.8f;
+
+    [SerializeField] [Range(0,1)]
+    private float dampFactor;
+    [SerializeField]
+    private float mass = 1;
+    [Range(0,1)]
+    [SerializeField]float coeficienteF;
+    float frontalArea;
+
+    private void Start()
+    {
+        frontalArea = transform.localScale.x;
+    }
+
+    private void Update()
+    {
+        // Reset accel
+        acceleration = Vector3.zero;
+
+        // Apply weight
+        ApplyForce(new Vector3(0, mass * gravity, 0));
+
+        if (transform.position.y < 0)
+        {
+            // Calculate the "fluid friction" force
+            Vector3 fluidFriction = ((velocity.magnitude * velocity.magnitude) * frontalArea 
+                * -coeficienteF * velocity.normalized) / 2;
+            ApplyForce(fluidFriction);
+        }
+
+        // Move the object
+        Move();
+        CheckLimits();
+    }
+
+    public void Move()
+    {
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+        // Debug my vectors
+        transform.position.Draw(Color.red);
+        acceleration.Draw(transform.position, Color.green);
+        velocity.Draw(transform.position, Color.blue);
+    }
+
+    private void CheckLimits()
+    {
+        Vector3 position = transform.position;
+        if ((position.x > 5 || position.x < -5))
+        {
+            if (position.x > 5) transform.position = new Vector3(5, transform.position.y);
+            if (position.x < -5) transform.position = new Vector3(-5, transform.position.y);
+
+            velocity.x = velocity.x * -1;
+            velocity *= dampFactor;
+        }
+        else if (position.y > 5 || position.y < -5)
+        {
+            if (position.y > 5) transform.position = new Vector3(transform.position.x, 5);
+            if (position.y < -5) transform.position = new Vector3(transform.position.x, -5);
+            velocity.y = velocity.y * -1;
+            velocity *= dampFactor;
+        }
+    }
+
+    private void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+}
